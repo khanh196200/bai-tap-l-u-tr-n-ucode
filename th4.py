5a505d1e4e44ddee0547c48c355704dac859e7b4
khanh196200 <62270609+khanh196200@users.noreply.github.com>
2020-05-11 21:38:56 +0700

    Add files via upload

---
+++ th4.py
@@ -0,0 +1,48 @@
+#cau9
+ds=input('nhap chuoi: ').split()
+#cau10
+ds=input('nhap chuoi: ').split()
+x=ds[0:1]
+for c in x:
+    print(c)
+
+#cau8
+    ds=input('word_list: ')
+def find_longest_word(word_list):
+    longest_word = ''
+    for word in word_list:
+        if len(word) > len(longest_word)
+            longest_word = word
+    print longest_word
+
+#cau7
+    s=input('chuoi kem so')
+result=''.join(i for i in s if not i.isdigit())
+result
+#cau6
+Ht=input('Ho ten la: ')
+print(Ht.split(' ', 1))
+print(Ht)
+#cau5
+ds=input('Danh sach: ')
+ds_dao_nguoc =''
+for letter in ds :
+    ds_dao_nguoc=letter + ds_dao_nguoc
+print('')
+print('Input:%s',ds)
+print('Output:%s',ds_dao_nguoc)
+#cau4
+ds=input('Danh sach:').split()
+print(ds)
+for so in ds:
+print(so)
+#cau3
+s=input("nhap chuoi: ")
+print(s.upper())
+print(s)
+#cau2
+a=int(input("nhap a: "))
+b=int(input("nhap b: "))
+file=open("add.txt","w")
+file.write(str(a+b))
+file.close()
